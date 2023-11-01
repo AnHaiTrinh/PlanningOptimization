db7a606dcc4d7c1faaeaf6146f0d8491489a8292
DESKTOP-R201FAL\Thinkpad <trinhanhai1710@gmail.com>
2023-11-01 18:31:34 +0700

    Add dynamic IP and CP solvers

--- src/utils/generation.py
+++ src/utils/generation.py
@@ -1,3 +1,9 @@
 def generate_real_subset(number):
     for i in range(1, (1 << number) - 1):
         yield [j for j in range(number) if i & (1 << j)]
+
+
+def generate_non_empty_subset(number_list):
+    n = len(number_list)
+    for i in range(1, (1 << n)):
+        yield [number_list[j] for j in range(n) if i & (1 << j)]
