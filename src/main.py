da7dabf243b93ccf6e8a8c5eb3a28ffc611131ad
thanhlam0241 <80621994+thanhlam0241@users.noreply.github.com>
2023-12-31 22:28:01 +0700

    log files

--- src/main.py
+++ src/main.py
@@ -10,6 +10,11 @@ if __name__ == '__main__':
         solver_factory = SolverFactory(n, k, costs, optimal)
         solver = solver_factory.create_solver(solver_name)
         solver.evaluate(return_time)
+
+        # Write log to file
+        f = open('logs.txt', 'a')
+        f.write("Solution: solve by [{}] for file [{}]\n".format(solver_name, os.path.basename(input_file)))
+        f.close()
     except Exception as e:
         print(e)
         exit(1)
--- src/solvers/bnb.py
+++ src/solvers/bnb.py
@@ -4,14 +4,23 @@ import numpy as np
 
 class BranchAndBoundSolver2(Solver):
     def solve(self) -> int:
+        # visited[i] = True nếu đã đi qua i, False nếu chưa
         visited = np.array([False] * (2 * self.n + 1))
+        # num_passengers = số khách trên xe
         num_passengers = 0
+        # min_distance = khoảng cách nhỏ nhất từ điểm hiện tại đến điểm cuối cùng
         min_distance = np.inf
+        # visited_last = điểm cuối cùng đã đi qua
         visited_last = 0
+        # current_distance = khoảng cách hiện tại từ điểm đầu đến điểm hiện tại
         current_distance = 0
+        # current_paths = đường đi hiện tại
         current_paths = [0] * (2 * self.n + 1)
+        # Khoảng cách nhỏ nhất giữa 2 vị trí khác nhau
         nonzero_values = self.costs[self.costs > 0]
         min_cost = np.min(nonzero_values) if len(nonzero_values) > 0 else None
+
+        # Kiểm tra xem có thể đi qua điểm hiện tại không
         def check_can_visit(current_pos):
             if visited[current_pos]:
                 return False
@@ -23,9 +32,11 @@ class BranchAndBoundSolver2(Solver):
                     return False
             return True
         
-        def check_can_bound(step):
+        # Kiểm tra xem có thể cắt nhánh không
+        def check_can_cut(step):
             return current_distance + min_cost * (2 * self.n - step) < min_distance
 
+        # Hàm quay lui
         def backtrack(step):
             nonlocal current_distance, min_distance, num_passengers, visited_last
             temp_visited_last = visited_last
@@ -33,7 +44,7 @@ class BranchAndBoundSolver2(Solver):
                 if current_distance + self.costs[visited_last][0] < min_distance:
                     min_distance = current_distance + self.costs[visited_last][0]
                 return
-            if check_can_bound(step):
+            if check_can_cut(step):
                 for i in range(1, 2 * self.n + 1):
                     if check_can_visit(i):
                         if i <= self.n:
@@ -54,4 +65,4 @@ class BranchAndBoundSolver2(Solver):
                             num_passengers += 1
 
         backtrack(0)
-        return int(min_distance)
+        return min_distance
