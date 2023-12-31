b4c83061a32a5b40225ea891f8db42594962a8e6
thanhlam0241 <80621994+thanhlam0241@users.noreply.github.com>
2023-12-31 14:22:03 +0700

    hello

--- src/solvers/ls.py
+++ src/solvers/ls.py
@@ -2,16 +2,12 @@ from .solver import Solver
 import numpy as np
 import random
 
-class LSSolver(Solver):
-    def solve(self) -> int:
-        MAX_INT = 1e9
-        n = self.n
-        k = self.k
-        costs = self.costs
+MAX_INT = 1e9
+def Greedy1(n,k,costs):
         # Init paths and visited array
         paths = [0] * (2 * n + 2)
         visited = [0] * (2 * n + 1) 
-        self.passengers = 0
+        passengers = 0
         for i in range(1, 2*n + 1):
             paths[i] = 0
             visited[i] = 0
@@ -19,9 +15,10 @@ class LSSolver(Solver):
 
         # Check if node can visit
         def check_can_visit(i):
+            nonlocal passengers
             if visited[i] == 1:
                 return False
-            if self.passengers == k:
+            if passengers == k:
                 if i <= n:
                     return False
                 else:
@@ -39,12 +36,13 @@ class LSSolver(Solver):
         
         # Visit node, update passengers and visited array
         def visit(order_node, value_position):
+            nonlocal passengers
             paths[order_node] = value_position
             visited[value_position] = 1
             if value_position > n:
-                self.passengers -= 1
+                passengers -= 1
             else:
-                self.passengers += 1
+                passengers += 1
         # Greedy algorithm
         def Greedy(i):
             min_length = MAX_INT
@@ -64,6 +62,125 @@ class LSSolver(Solver):
                 # print(paths)
                 # print(cost_paths)
         Greedy(1)
+        return paths
+
+def Greedy2(n,k,costs):
+    def weight_j(r, i):
+        ln = -np.log(r+1e-5)
+        if ln < 1e-4:
+            return 0
+        return np.power(ln, 1 + 50*np.sqrt(i/n))
+
+    def valid(sol, city, visited):
+        #check if node is visited
+        if visited[city] == 1:
+            return False
+
+        next_sol = sol.copy()
+        next_sol.append(city)
+
+        mark = [0]*(n+1)
+        n_passengers = 0
+        for _city in next_sol[1:]:
+            if _city <= n:
+                n_passengers += 1
+                mark[_city] = 1
+            else:
+                n_passengers -= 1
+                mark[_city-n] = 0
+            if n_passengers > k:
+                return False
+
+        if np.sum(mark)-n_passengers != 0:
+            return False
+        return True
+
+    def solve():
+        solution = []
+        solution.append(0) # start at city 0
+        fitness = 0 # objective value
+        visited = [0]*(2*n+1)
+        visited[0] = 1
+        for _ in range(1, 2*n + 1):
+
+            #current city
+            c_city = solution[-1]
+
+            weights = [] # probability distribution
+            possible_move = []
+            d_max = 0
+
+            for city in range(1, 2*n+1):
+                if valid(solution, city, visited) == False:
+                    continue
+                d_i = costs[c_city][city]
+                possible_move.append([city, d_i]) #[city, distance]
+                # if d_i > d_max:
+                #     d_max = d_i
+
+            possible_move = sorted(possible_move, key=lambda x: x[1])
+
+            if len(possible_move) > 20:
+                possible_move = possible_move[:20]
+
+            for city in possible_move:
+                if d_max < city[1]: d_max=city[1]
+
+            # print(solution)
+            # print(possible_move)
+            # print(weights)
+
+            if d_max == 0:
+                next_city = possible_move[0]
+            else:
+                # cal weights
+                for move in possible_move:
+                    r = move[1]/d_max
+                    weights.append(weight_j(r, _))
+
+                if np.sum(weights) == 0:
+                    next_city = possible_move[0]
+                else:
+                    # choices next city
+                    next_city = random.choices(possible_move, k=1, weights=weights)[0]
+
+            #update solution
+            solution.append(next_city[0])
+
+            #mark visited
+            visited[next_city[0]] = 1
+
+            fitness += next_city[1]
+
+        return solution, fitness
+
+    def solver():
+        record = 10e9
+        best_sol = None
+        for seed in range(10):
+            random.seed(seed)
+            sol, fit = solve()
+            # print(fit + costs[sol[-1]][0])
+            if record > fit:
+                record = fit
+                best_sol = sol.copy()
+        return best_sol, record
+    
+    best_sol, best_fitness = solver()
+
+    # print("Objective value: ", best_fitness + costs[best_sol[-1]][0])
+    # print("Solution: ", best_sol)
+    return best_sol
+
+class LSSolver(Solver):
+    def solve(self) -> int:
+        k = self.k
+        n = self.n
+        costs = self.costs
+
+        # Greedy algorithm
+        paths = Greedy2(n, k, costs)
+
         sum = 0
         for i in range(1, 2*n + 1):
             sum += costs[paths[i-1]][paths[i]]
@@ -153,5 +270,6 @@ class LSSolver(Solver):
                     swap(random_node, node_swap, current_paths)
                     self.current_cost_path = max_costs
             # print(current_cost_path)
-        local_search(15000)
+        local_search(100)
         return self.current_cost_path
+    
